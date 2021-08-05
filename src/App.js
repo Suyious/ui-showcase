2eb8cd8bbce9bfaa1e69ec788869abbe9eeef637
Suyash <suyu162001@gmail.com>
2021-08-06 00:02:20 +0530

    still fiddling over deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, {Suspense} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Carousel from "./components/carousel/Carousel";
-import Home from "./components/Home";
-// import Fallback from "./components/Fallback"
+//import Carousel from "./components/carousel/Carousel";
+//import Home from "./components/Home";
+import Fallback from "./components/Fallback"
 
-// const Carousel = React.lazy(()=>import("./components/carousel/Carousel"))
-// const Home = React.lazy(()=>import("./components/Home"))
+const Carousel = React.lazy(()=>import("./components/carousel/Carousel"))
+const Home = React.lazy(()=>import("./components/Home"))
 
 function App() {
   return (
     <div className="App">
-      {/* <Suspense fallback={Fallback}> */}
+      <Suspense fallback={Fallback}>
         <Router>
           <Route exact path="/" component={Home} />
           <Route exact path="/carousel" component={Carousel} />
         </Router>
-      {/* </Suspense> */}
+      </Suspense>
     </div>
   );
 }

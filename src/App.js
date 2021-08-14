e4f0d822a66f22517897e9b9305a9457abf9c6cd
Suyash <suyu162001@gmail.com>
2021-08-15 01:04:47 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Suspense} from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 //import Carousel from "./components/carousel/Carousel";
 //import Home from "./components/Home";
@@ -13,9 +13,11 @@ function App() {
     <div className="App">
       <Router basename="/ui-showcase">
         <Suspense fallback={Fallback()}>
-          <Route exact path="/" component={Home} />
-          <Route path="/carousel" component={Carousel} />
-          <Route path="/fallbackpreview" component={Fallback}/>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/carousel" component={Carousel} />
+            <Route path="/fallbackpreview" component={Fallback}/>
+          </Switch>
         </Suspense>
       </Router>
     </div>

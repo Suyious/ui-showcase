f2a171b6c269f9b1dab1d480bd1fc53ddd2d9e2b
Suyash <suyu162001@gmail.com>
2021-08-15 23:35:23 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Suspense} from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 //import Carousel from "./components/carousel/Carousel";
 //import Home from "./components/Home";
